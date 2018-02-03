c8eeecbd5a0c15d8f47e80627d8f466ead4a478b
Pepe Valdivia <jvaldivia@softweb.pe>
2018-02-03 13:10:38 -0500

    Primer route

--- app.lua
+++ app.lua
@@ -1,9 +1,16 @@
 local lapis = require("lapis")
 local config = require("lapis.config").get()
+local respond_to = require("lapis.application").respond_to
 local app = lapis.Application()
+
+-- LOGIN Y ERRORES ... ROUTES
+local login = require("routes.login")
 
 app:get("/", function()
   return "Welcome to Lapis xd " .. require("lapis.version")
 end)
+
+-- LOGIN Y ERRORES ... RUTAS
+app:match("homeIndex", "/login", respond_to(login.Index(self))) 
 
 return app
---
+++ routes/login.lua
@@ -0,0 +1,18 @@
+local M = {}
+local config = require("lapis.config")
+local inspect = require('inspect')
+
+local function Index(self)
+  return {
+    before = function(self)
+
+    end,
+    GET = function(self)
+      return 'login'
+    end
+  }
+end
+
+M.Index = Index
+
+return M
