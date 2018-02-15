b8f5fbf9358e78a6aaeeca8b2272a5942ae528ea
Pepe Valdivia <jvaldivia@softweb.pe>
2018-02-15 17:40:01 -0500

    CRSF middel habilidtado

--- config/middleware.lua
+++ config/middleware.lua
@@ -2,7 +2,7 @@ local M = {}
 local constants = require('config.constants')
 
 local function ValidarCSRF(self)
-  if self.params['csrf'] ~= 'constants.CSRF' then
+  if self.params['csrf'] ~= constants.CSRF then
     self:write({redirect_to = constants.BASE_URL .. 'error/access/505'})
   end
 end
