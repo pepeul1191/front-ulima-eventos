c859bf1c375eff6b3c4017f28f3fc162b1d8b592
Pepe Valdivia <jvaldivia@softweb.pe>
2018-02-15 14:42:07 -0500

    Uso de helpers para cargar archivos estáticos

--- routes/login.lua
+++ routes/login.lua
@@ -2,6 +2,7 @@ local M = {}
 local config = require('lapis.config')
 local inspect = require('inspect')
 local constants = require('config.constants')
+local helpers = require('config.helpers')
 
 local function Index(self)
   return {
@@ -10,6 +11,10 @@ local function Index(self)
     end,
     GET = function(self)
       self.constants = constants
+      self.helpers = helpers
+      self.csss = {'bower_components/bootstrap/dist/css/bootstrap.min', 'bower_components/font-awesome/css/font-awesome.min'}
+      self.jss = {'bower_components/jquery/dist/jquery.min', 'bower_components/bootstrap/dist/js/bootstrap.min'}
+      self.title = 'Login'
       return { render = 'login.index', layout = 'layouts.blank'}
     end
   }
