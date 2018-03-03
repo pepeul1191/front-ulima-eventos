520f7ac587e89195be59acd470fed1d776e6e535
Pepe Valdivia <jvaldivia@softweb.pe>
2018-03-03 06:54:10 -0500

    Servidor de archivos estáticos apunta a un de desarrollo

--- config/constants.lua
+++ config/constants.lua
@@ -1,7 +1,7 @@
 local M = {}
 
-M.BASE_URL = 'http://localhost:8080/'
-M.STATIC_URL = 'http://localhost:81/animalitos-static/'
+M.BASE_URL = 'http://localhost:9090/'
+M.STATIC_URL = 'http://localhost:9100/'
 M.CSRF = 'TLXTtwXA-_et0s5uKPPcAGm5dtQIAjXgHmmM'
 M.SERVICIOS = {
   accesos = 'http://localhost:5000/',
